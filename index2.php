b86e1db5364cb809e924e646d7ff1f6c74a43ba8
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-09-15 21:53:14 +0200

    On the class state

--- index2.php
+++ index2.php
@@ -3,12 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.9, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP Basics</title>
   </head>
   <body>
     <h1>
-      <?= 'Hello' . $_Get['name'];?>
+      <?= 'Hello' . htmlspecialchars($_Get['name']);?>
     </h1>
   </body>
 </html>
