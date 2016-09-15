326b099a276f6c1066489c354653282f3fcf2b81
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-09-15 21:43:31 +0200

    Adding an example with html

---
+++ index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Basics</title>
+  </head>
+  <body>
+    <?php
+    $greetings = "Hello";
+    
+    ?>
+  </body>
+</html>
