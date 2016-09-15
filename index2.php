9a92e50bae712718ca669c9e0c22a53b5165a409
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-09-15 21:51:54 +0200

    Setted up the minimum html file with php

--- index2.php
+++ index2.php
@@ -2,12 +2,13 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.9, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
     <title>PHP Basics</title>
   </head>
   <body>
-    <?php
-    $greetings = "Hello";
-    
-    ?>
+    <h1>
+      <?= 'Hello' . $_Get['name'];?>
+    </h1>
   </body>
 </html>
