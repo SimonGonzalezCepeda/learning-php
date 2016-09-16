3533f9486c9627cc473a22c8b123cad82399ebcc
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-09-16 17:33:33 +0200

    Testing

--- index2.php
+++ index2.php
@@ -1,5 +1,14 @@
 <?php
 
+
+$people = [
+    $name => 'Simón',
+    $sn1 => 'González',
+    $sn2 => 'Cepeda',
+    $age => 24,
+    $married => true
+];
+
 var_dump();
 
-require 'index-template.php';
+//require 'index-template.php';
