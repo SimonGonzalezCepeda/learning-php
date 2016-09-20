d88598954e23ec076c8d3d00757e114bedf454d9
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-09-20 17:47:03 +0200

    Introducing PHP classes

---
+++ task.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'functions.php';
+
+class Task {
+
+    public function __construct()
+    {
+        echo "Nova tasca construida";
+    }
+
+//    public function Task(){
+//
+//    }
+
+}
+$task = new Task;
+
+var_dump($task);
